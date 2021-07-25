08d7f10335b4577c709dddb77ae14823739e913f
sungsu.park <sungsu.park@webtoonscorp.com>
2021-07-25 16:06:27 +0900

    add rest api

--- src/main/kotlin/com/example/starter/web/home/HomController.kt
+++ src/main/kotlin/com/example/starter/web/home/HomController.kt
@@ -1,7 +1,11 @@
 package com.example.starter.web.home
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class HomeController(private val homeService: HomeService) {
@@ -12,3 +16,20 @@ class HomeController(private val homeService: HomeService) {
 		return "home/home";
 	}
 }
+
+@RestController
+class HomeRestController(private val homeService: HomeService) {
+
+	@GetMapping(value = ["/home"])
+	fun getHome(): ResponseEntity<HomeResult> {
+		val items = listOf("abc","def","ghi")
+		return ResponseEntity(HomeResult(items, items.size), HttpStatus.OK)
+	}
+
+}
+
+data class HomeResult(
+		var items: List<String>,
+		var count: Int
+
+)
